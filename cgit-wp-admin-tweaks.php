16a67fdd26961a96327b09f370d4ea088bfc5195
John Hughes <john@castlegateit.co.uk>
2014-10-02 17:08:54 +0100

    Initial commit

---
+++ cgit-wp-admin-tweaks.php
@@ -0,0 +1,179 @@
+<?php
+
+/*
+
+Plugin Name: Castlegate IT WP Admin Tweaks
+Plugin URI: http://github.com/castlegateit/cgit-wp-contact-form
+Description: Minor tweaks to the WordPress admin panel.
+Version: 1.0
+Author: Castlegate IT
+Author URI: http://www.castlegateit.co.uk/
+License: MIT
+
+*/
+
+/**
+ * Includes
+ */
+require_once dirname( __FILE__ ) . '/settings.php';
+
+/**
+ * Hide admin toolbar
+ *
+ * Hide the admin toolbar for all users. This will override any per-user admin
+ * toolbar settings.
+ */
+if ( get_option('cgit_admin_hide_toolbar') ) {
+    show_admin_bar(FALSE);
+}
+
+/**
+ * Edit welcome message
+ *
+ * Replace "Howdy" with something a bit more professional or nothing at all.
+ */
+function cgit_admin_edit_welcome_message ($wp_admin_bar) {
+
+    $account = $wp_admin_bar->get_node('my-account');
+    $message = get_option('cgit_admin_welcome_message');
+    $title   = str_replace( 'Howdy,', $message, $account->title );
+    $node    = array( 'id' => 'my-account', 'title' => $title );
+
+    $wp_admin_bar->add_node($node);
+
+}
+
+if ( get_option('cgit_admin_edit_welcome_message') ) {
+    add_filter('admin_bar_menu', 'cgit_admin_edit_welcome_message', 25);
+}
+
+/**
+ * Hide menu items
+ */
+function cgit_admin_hide_menu_items () {
+
+    if ( ! current_user_can('manage_options') ) {
+
+        if ( get_option('cgit_admin_hide_menu_posts') )      remove_menu_page('edit.php');                // Posts
+        if ( get_option('cgit_admin_hide_menu_media') )      remove_menu_page('upload.php');              // Media
+        if ( get_option('cgit_admin_hide_menu_links') )      remove_menu_page('link-manager.php');        // Links
+        if ( get_option('cgit_admin_hide_menu_pages') )      remove_menu_page('edit.php?post_type=page'); // Pages
+        if ( get_option('cgit_admin_hide_menu_comments') )   remove_menu_page('edit-comments.php');       // Comments
+        if ( get_option('cgit_admin_hide_menu_profile') )    remove_menu_page('profile.php');             // Profile
+        if ( get_option('cgit_admin_hide_menu_tools') )      remove_menu_page('tools.php');               // Tools
+
+        if ( get_option('cgit_admin_hide_menu_categories') ) remove_submenu_page('edit.php', 'edit-tags.php?taxonomy=category'); // Posts | Categories
+        if ( get_option('cgit_admin_hide_menu_tags') )       remove_submenu_page('edit.php', 'edit-tags.php?taxonomy=post_tag'); // Posts | Tags
+
+    }
+
+}
+
+add_action('admin_menu', 'cgit_admin_hide_menu_items');
+
+/**
+ * Hide update notifications
+ */
+function cgit_admin_hide_update_notifications () {
+    if ( ! current_user_can('manage_options') ) {
+        remove_action('admin_notices', 'update_nag', 3);
+    }
+}
+
+if ( get_option('cgit_admin_hide_update_notifications') ) {
+    add_action('admin_menu', 'cgit_admin_hide_update_notifications');
+}
+
+/**
+ * Hide media controls
+ *
+ * Prevents users from inserting inline media in the content editor by removing
+ * the media button.
+ */
+function cgit_admin_hide_media_buttons () {
+    remove_action('media_buttons', 'media_buttons');
+}
+
+if ( get_option('cgit_admin_hide_media_buttons') ) {
+    add_action('admin_head', 'cgit_admin_hide_media_buttons');
+}
+
+/**
+ * Hide editor buttons
+ *
+ * Removes presentational HTML buttons from the TinyMCE content editor. See
+ * http://www.tinymce.com/wiki.php/Buttons/controls for a complete list of
+ * buttons.
+ */
+function cgit_admin_hide_editor_buttons ($buttons) {
+
+    $old_buttons = array('forecolor', 'indent', 'justifycenter', 'justifyfull', 'justifyleft', 'justifyright', 'outdent', 'strikethrough', 'underline', 'wp_more');
+    $new_buttons = array_diff($buttons, $old_buttons);
+
+    return $new_buttons;
+
+}
+
+if ( get_option('cgit_admin_hide_editor_buttons') ) {
+    add_filter('mce_buttons', 'cgit_admin_hide_editor_buttons');
+    add_filter('mce_buttons_2', 'cgit_admin_hide_editor_buttons');
+}
+
+/**
+ * Hide additional block level elements in editor
+ *
+ * Restricts block level elements to <p>, <h2>, <h3>, and <h4> in the content
+ * editor.
+ */
+function cgit_admin_hide_block_elements ($init) {
+    $init['block_formats'] = 'Paragraph=p;Heading 2=h2;Heading 3=h3;Heading 4=h4';
+    return $init;
+}
+
+if ( get_option('cgit_admin_hide_block_elements') ) {
+    add_filter('tiny_mce_before_init', 'cgit_admin_hide_block_elements');
+}
+
+/**
+ * Force plain text paste
+ *
+ * Prevent pasting of formatted text in the content editor. Based on code by
+ * Till Kruess at http://www.tillkruess.com.
+ */
+function cgit_admin_force_plain_text ($init) {
+
+    global $tinymce_version;
+
+    if ( $tinymce_version[0] < 4 ) {
+        $init['paste_text_sticky'] = TRUE;
+        $init['paste_text_sticky_default'] = TRUE;
+    } else {
+        $init['paste_as_text'] = TRUE;
+    }
+
+    return $init;
+
+}
+
+function cgit_admin_load_paste_in_teeny ($plugins) {
+    $plugins[] = 'paste';
+    return $plugins;
+}
+
+function cgit_admin_hide_plain_text_button ($buttons) {
+
+    $old_buttons = array('pastetext');
+    $new_buttons = array_diff($buttons, $old_buttons);
+
+    return $new_buttons;
+
+}
+
+if ( get_option('cgit_admin_force_plain_text') ) {
+    add_filter('tiny_mce_before_init', 'cgit_admin_force_plain_text');
+    add_filter('teeny_mce_before_init', 'cgit_admin_force_plain_text');
+    add_filter('teeny_mce_plugins', 'cgit_admin_load_paste_in_teeny');
+    add_filter('teeny_mce_plugins', 'cgit_admin_load_paste_in_teeny');
+    add_filter('mce_buttons', 'cgit_admin_hide_plain_text_button');
+    add_filter('mce_buttons_2', 'cgit_admin_hide_plain_text_button');
+}
---
+++ settings.php
@@ -0,0 +1,254 @@
+<?php
+
+/**
+ * Register settings
+ */
+function cgit_admin_register_settings () {
+
+    register_setting('cgit_admin', 'cgit_admin_hide_toolbar');
+    register_setting('cgit_admin', 'cgit_admin_edit_welcome_message');
+    register_setting('cgit_admin', 'cgit_admin_welcome_message');
+    register_setting('cgit_admin', 'cgit_admin_hide_menu_posts');
+    register_setting('cgit_admin', 'cgit_admin_hide_menu_media');
+    register_setting('cgit_admin', 'cgit_admin_hide_menu_links');
+    register_setting('cgit_admin', 'cgit_admin_hide_menu_pages');
+    register_setting('cgit_admin', 'cgit_admin_hide_menu_comments');
+    register_setting('cgit_admin', 'cgit_admin_hide_menu_categories');
+    register_setting('cgit_admin', 'cgit_admin_hide_menu_tags');
+    register_setting('cgit_admin', 'cgit_admin_hide_menu_profile');
+    register_setting('cgit_admin', 'cgit_admin_hide_menu_tools');
+    register_setting('cgit_admin', 'cgit_admin_hide_update_notifications');
+    register_setting('cgit_admin', 'cgit_admin_hide_media_buttons');
+    register_setting('cgit_admin', 'cgit_admin_hide_editor_buttons');
+    register_setting('cgit_admin', 'cgit_admin_hide_block_elements');
+    register_setting('cgit_admin', 'cgit_admin_force_plain_text');
+
+}
+
+/**
+ * Add settings page
+ */
+function cgit_admin_add_settings_page () {
+
+    add_submenu_page('options-general.php', 'WP Admin Tweaks', 'WP Admin Tweaks', 'manage_options', 'cgit-wp-admin-tweaks', 'cgit_admin_settings_page');
+    add_action('admin_init', 'cgit_admin_register_settings');
+
+}
+
+add_action('admin_menu', 'cgit_admin_add_settings_page');
+
+/**
+ * Render settings page
+ */
+function cgit_admin_settings_page () {
+
+    ?><div class="wrap">
+
+        <h2>WP Admin Tweaks</h2>
+
+        <form action="options.php" method="post">
+
+            <?php settings_fields('cgit_admin'); ?>
+
+            <h3>Interface</h3>
+
+            <table class="form-table">
+
+                <tr>
+                    <th>
+                        Hide toolbar
+                    </th>
+                    <td>
+                        <label>
+                            <input type="checkbox" name="cgit_admin_hide_toolbar" value="1"<?php echo get_option('cgit_admin_hide_toolbar') ? ' checked="checked"' : ''; ?> />
+                            Hide toolbar for all users when viewing the site
+                        </label>
+                    </td>
+                </tr>
+
+                <tr>
+                    <th>
+                        Edit welcome message
+                    </th>
+                    <td>
+                        <label>
+                            <input type="checkbox" name="cgit_admin_edit_welcome_message" value="1"<?php echo get_option('cgit_admin_edit_welcome_message') ? ' checked="checked"' : ''; ?> />
+                            Edit the welcome message (default "Howdy,")
+                        </label>
+                    </td>
+                </tr>
+
+                <tr>
+                    <th>
+                        <label for="welcome_message">Welcome message text</label>
+                    </th>
+                    <td>
+                        <input type="text" name="welcome_message" id="cgit_admin_welcome_message" value="<?php echo htmlspecialchars(get_option('cgit_admin_welcome_message')); ?>" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <th rowspan="5">
+                        Hide main menus
+                    </th>
+                    <td>
+                        <label>
+                            <input type="checkbox" name="cgit_admin_hide_menu_posts" value="1"<?php echo get_option('cgit_admin_hide_menu_posts') ? ' checked="checked"' : ''; ?> />
+                            Posts
+                        </label>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <label>
+                            <input type="checkbox" name="cgit_admin_hide_menu_media" value="1"<?php echo get_option('cgit_admin_hide_menu_media') ? ' checked="checked"' : ''; ?> />
+                            Media
+                        </label>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <label>
+                            <input type="checkbox" name="cgit_admin_hide_menu_links" value="1"<?php echo get_option('cgit_admin_hide_menu_links') ? ' checked="checked"' : ''; ?> />
+                            Links
+                        </label>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <label>
+                            <input type="checkbox" name="cgit_admin_hide_menu_pages" value="1"<?php echo get_option('cgit_admin_hide_menu_pages') ? ' checked="checked"' : ''; ?> />
+                            Pages
+                        </label>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <label>
+                            <input type="checkbox" name="cgit_admin_hide_menu_comments" value="1"<?php echo get_option('cgit_admin_hide_menu_comments') ? ' checked="checked"' : ''; ?> />
+                            Comments
+                        </label>
+                    </td>
+                </tr>
+
+                <tr>
+                    <th rowspan="2">
+                        Hide taxonomy menus
+                    </th>
+                    <td>
+                        <label>
+                            <input type="checkbox" name="cgit_admin_hide_menu_categories" value="1"<?php echo get_option('cgit_admin_hide_menu_categories') ? ' checked="checked"' : ''; ?> />
+                            Categories
+                        </label>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <label>
+                            <input type="checkbox" name="cgit_admin_hide_menu_tags" value="1"<?php echo get_option('cgit_admin_hide_menu_tags') ? ' checked="checked"' : ''; ?> />
+                            Tags
+                        </label>
+                    </td>
+                </tr>
+
+                <tr>
+                    <th rowspan="2">
+                        Hide options menus
+                    </th>
+                    <td>
+                        <label>
+                            <input type="checkbox" name="cgit_admin_hide_menu_profile" value="1"<?php echo get_option('cgit_admin_hide_menu_profile') ? ' checked="checked"' : ''; ?> />
+                            Profile
+                        </label>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <label>
+                            <input type="checkbox" name="cgit_admin_hide_menu_tools" value="1"<?php echo get_option('cgit_admin_hide_menu_tools') ? ' checked="checked"' : ''; ?> />
+                            Tools
+                        </label>
+                    </td>
+                </tr>
+
+            </table>
+
+            <h3>Notifications</h3>
+
+            <table class="form-table">
+
+                <tr>
+                    <th>
+                        Hide notifications
+                    </th>
+                    <td>
+                        <label>
+                            <input type="checkbox" name="cgit_admin_hide_update_notifications" value="1"<?php echo get_option('cgit_admin_hide_update_notifications') ? ' checked="checked"' : ''; ?> />
+                            Hide update notifications for non-admin users
+                        </label>
+                    </td>
+                </tr>
+
+            </table>
+
+            <h3>Editor</h3>
+
+            <table class="form-table">
+
+                <tr>
+                    <th rowspan="3">
+                        Hide editor controls
+                    </th>
+                    <td>
+                        <label>
+                            <input type="checkbox" name="cgit_admin_hide_media_buttons" value="1"<?php echo get_option('cgit_admin_hide_media_buttons') ? ' checked="checked"' : ''; ?> />
+                            Media button
+                        </label>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <label>
+                            <input type="checkbox" name="cgit_admin_hide_editor_buttons" value="1"<?php echo get_option('cgit_admin_hide_editor_buttons') ? ' checked="checked"' : ''; ?> />
+                            Presentational markup controls
+                        </label>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <label>
+                            <input type="checkbox" name="cgit_admin_hide_block_elements" value="1"<?php echo get_option('cgit_admin_hide_block_elements') ? ' checked="checked"' : ''; ?> />
+                            Extra block level elements (<code>h1</code>, <code>pre</code>, <code>address</code>, etc.)
+                        </label>
+                    </td>
+                </tr>
+
+                <tr>
+                    <th>
+                        Paste as text
+                    </th>
+                    <td>
+                        <label>
+                            <input type="checkbox" name="cgit_admin_force_plain_text" value="1"<?php echo get_option('cgit_admin_force_plain_text') ? ' checked="checked"' : ''; ?> />
+                            Force paste as plain text
+                        </label>
+                    </td>
+                </tr>
+
+            </table>
+
+            <?php submit_button(); ?>
+
+        </form>
+
+    </div><?php
+
+}
